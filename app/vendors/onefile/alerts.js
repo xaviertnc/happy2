e7929c53e26782d9fa10e795fcbc2cae3229edc4
xaviertnc <xavier.tnc@gmail.com>
2020-09-23 20:16:19 +0200

    v0.5.0.3 - FIX - alerts.js: Fix incomplete add() method. Rename mount() to activateAlert().

--- app/vendors/onefile/alerts.js
+++ app/vendors/onefile/alerts.js
@@ -11,17 +11,22 @@ window.F1 = window.F1 || { afterPageLoadScripts: [] };
  *
  */
 
-F1.Alerts = function (alertsContainerSelector, options)
+F1.Alerts = function (containerSelector, options)
 {
   options = options || {};
-  this.alertsSelector = alertsContainerSelector || '#alerts';
+  this.alertsContainerSelector = containerSelector || '#alerts';
   this.alertSelector = '.alert';
   this.fadeDuration = 2000;
   Object.assign(this, options);
 };
 
 
-F1.Alerts.prototype.mount = function(elAlert) {
+F1.Alerts.prototype.removeAlert = function(elAlert) {
+  elAlert.parentElement.removeChild(elAlert);
+}
+
+
+F1.Alerts.prototype.activateAlert = function(elAlert) {
   let ttlTimer;
   const self = this;
   const ttl = parseInt(elAlert.getAttribute('data-ttl'));
@@ -29,31 +34,29 @@ F1.Alerts.prototype.mount = function(elAlert) {
     ttlTimer = setTimeout(function() {
       elAlert.style.transition = 'opacity ' + self.fadeDuration + 'ms';
       elAlert.style.opacity = 0;
-      ttlTimer = setTimeout(function() {
-        elAlert.parentElement.removeChild(elAlert);
-      }, self.fadeDuration);
+      ttlTimer = setTimeout(function() { self.removeAlert(elAlert); }, self.fadeDuration);
     }, ttl);
   }
   elAlert.addEventListener('click', function() {
-    clearTimeout(ttlTimer);
-    elAlert.parentElement.removeChild(elAlert);
+    clearTimeout(ttlTimer); self.removeAlert(elAlert);
   });
 };
 
 
-F1.Alerts.prototype.add = function(message, type, ttl) {
+F1.Alerts.prototype.add = function(type, message, ttl) {
   let self = this, elAlert = document.createElement('div');
   elAlert.setAttribute('data-ttl', ttl);
   elAlert.className = 'alert ' + type;
   elAlert.innerHTML = message;
-  elAlert.addEventListener('click', function() { self.mount(elAlert); });
+  this.el.appendChild(elAlert);
+  this.activateAlert(elAlert);
 };
 
 
 F1.Alerts.prototype.init = function() {
   let self = this;
-  this.el = document.querySelector(this.alertsSelector);
-  this.el.querySelectorAll(this.alertSelector).forEach(elAlert => self.mount(elAlert));
+  this.el = document.querySelector(this.alertsContainerSelector);
+  this.el.querySelectorAll(this.alertSelector).forEach(elAlert => self.activateAlert(elAlert));
   F1.console.log('F1 Alerts Initialized');
 };
 
